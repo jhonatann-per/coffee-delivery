f086ed0b13ca3fc21d4b0b12b08d6b9b412bc899
jhonatann-per <jhonatanpereira152@gmail.com>
2025-02-16 23:09:45 -0300

    Cria a estrutura do campo de CEP, pagamento e produtos escolhidos sem estilização.

--- src/pages/Carrinho/index.jsx
+++ src/pages/Carrinho/index.jsx
@@ -1,16 +1,55 @@
-import { Container } from "./styles"
+import { Container, EnderecoForm, PedidoContent } from "./styles"
 
 export const Carrinho = () => {
     return(
         <Container>
-            <div>
-                <section>Campo Endereço</section>
+            <EnderecoForm>
+                <header>Complete seu pedido</header>
+                <section>
+                    <div>
+                        <p>Endereço de Entrega</p>
+                        <span>Informe o endereço onde deseja receber seu pedido</span>
+                    </div>
+                    <form>
+                            <input type="number" name="cep" placeholder="CEP" />
+                            <input type="text" name="rua" placeholder="Rua" />
+                            <input type="number" name="numero" placeholder="Número" />
+                            <input type="text" name="complemento" placeholder="Complemento" />
+                            <input type="text" name="bairro" placeholder="Bairro" />
+                            <input type="text" name="cidade" placeholder="Cidade" />
+                            <input type="text" name="uf" placeholder="UF" />
+                    </form>
+                </section>
                 <article>
-                    Metodo De Pagamento
+                    <p>Pagamento</p>
+                    <span>O pagamento é feito na entrega. Escolha a forma que deseja pagar</span>
+                    <div>
+                        <button>Cartão de crédito</button>
+                        <button>Cartão de crédito</button>
+                        <button>Cartão de crédito</button>
+                    </div>
+                    
                 </article>
-            </div>
+            </EnderecoForm>
             
-            <aside>Produtos e total</aside>
+            <PedidoContent>
+                <header>Cafés selecionados</header>
+                <section>
+                    <div>Produtos do carrinho</div>
+                    <div>
+                        <div>
+                            <p>Total de itens</p>
+                        </div>
+                        <div>
+                            <p>Entrega</p>
+                        </div>
+                        <div>
+                            <strong>Total</strong>
+                        </div>
+                    </div>
+                    <button>Confirmar pedido</button>
+                </section>
+            </PedidoContent>
             
         </Container>
     )
--- src/pages/Carrinho/styles.jsx
+++ src/pages/Carrinho/styles.jsx
@@ -6,22 +6,69 @@ export const Container = styled.div`
 
     display: flex;
     flex-direction: row;
-    justify-content: space-between;
+    gap: 2rem;
 
-    div {
+`;
+
+export const EnderecoForm = styled.div`
         width: 40rem;
         display: flex;
         flex-direction: column;
         gap: 16px;
         
-    }
+    
     section {
         width: 100%;
         height: 372px;
         background-color: ${({theme}) => theme.colors['base-card']};
         border-radius: 4px;
-        padding: 5rem;
-        
+        padding: 2rem;
+        display: flex;
+        flex-direction: column;
+       
+        div {
+            width: 100%;
+            margin-bottom: 1rem;
+            display: flex;
+            flex-direction: column;
+            align-items: flex-start;
+        }
+
+        form {
+            display: grid;
+            grid-template-columns: repeat(3, 1fr);
+            gap: 10px;
+
+            
+            input:nth-child(1) {
+                grid-column: 1 / span 1;
+            }
+
+            input:nth-child(2) {
+                grid-column: 1 / span 3;
+                width: 100%;
+            }
+
+            input:nth-child(3) {
+                grid-column: 1 / span 1;
+            }
+            
+            input:nth-child(4) {
+                grid-column: 2 / span 2;
+            }
+            
+            input:nth-child(5) {
+                grid-column: 1 / span 1;
+            }
+            
+            input:nth-child(6) {
+                grid-column: 2 / span 1;
+            }
+            
+            input:nth-child(7) {
+                grid-column: 3 / span 1;
+            }
+        }
     }
 
     article {
@@ -31,12 +78,21 @@ export const Container = styled.div`
         border-radius: 4px;
         padding: 5rem;
     }
+`;
 
-    aside {
-        width: 28rem;
-        height: 498px;
-        background-color: ${({theme}) => theme.colors['base-card']};
-        border-radius: 4px;
-        padding: 5rem;
-    }
+export const PedidoContent = styled.div`
+        display: flex;
+        flex-direction: column;
+        gap: 16px;
+        
+        
+        section {
+            width: 28rem;
+            height: 498px;
+            background-color: ${({theme}) => theme.colors['base-card']};
+            border-radius: 4px;
+            padding: 5rem;
+        }
+       
+        
 `;
